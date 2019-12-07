5e486499a65efdd5d897c2934e091738f5173bfb
Gavin-JH <gavinjh@163.com>
2019-12-07 16:42:57 +0800

    Helloworld

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class Hello{
+        public static void main(String[] args) {
+	System.out.println("HelloWorld1");
+                System.out.println("HelloWorld2");
+                System.out.println("HelloWorld3");
+                System.out.println("HelloWorld4");
+       }
+}
